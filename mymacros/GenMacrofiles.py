396f82406afa8dc0a4b328b2fe2e12ceb835362d
Samu90 <torelli.1654130@studenti.uniroma1.it>
2023-05-15 21:23:38 +0200

    update macro file generation

--- mymacros/GenMacrofiles.py
+++ mymacros/GenMacrofiles.py
@@ -19,6 +19,95 @@ def GEMsGen():
             f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
             f.write("/run/beamOn 10000000"+"\n")
 
+def GEMsGenOuter():
+
+    ElementGems=["K40","Co60","Cs137"]
+    ZElementGems=[19,27,55]
+    NElements=[40,60,137]
+    RadEl="GEMsOuter"
+    
+    for i in range(len(ElementGems)):
+        with open(RadEl+"_"+ElementGems[i]+".mac", 'w') as f:
+            f.write("/process/eLoss/StepFunction 0.01 0.1 mm"+"\n")
+            f.write("/run/verbose 0"+"\n")
+            f.write("/event/verbose 0"+"\n")
+            f.write("/random/setSeeds 575223199 8381234975"+"\n")
+            f.write("/output/OutFile "+RadEl+"_"+ElementGems[i]+"\n")
+            f.write("#Elements: Cathodes GEMs Rings Vessel Lens Sensors"+"\n")
+            f.write("/detector/RadElement "+RadEl+"\n")
+            f.write("/isotope/AtomicNumber "+str(ZElementGems[i])+"\n")
+            f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
+            f.write("/run/beamOn 10000000"+"\n")
+
+def GEMsGenOuterUTh():
+
+    ElementGems=["U238","Th232"]
+    ZElementGems=[92,90]
+    NElements=[238,232]
+    RadEl="GEMsOuter"
+    
+    for i in range(len(ElementGems)):
+        for j in range(10):
+            with open(RadEl+'_'+ElementGems[i]+"_"+str(j)+".mac", 'w') as f:
+                f.write("/process/eLoss/StepFunction 0.01 0.1 mm"+"\n")
+                f.write("/run/verbose 0"+"\n")
+                f.write("/event/verbose 0"+"\n")
+                f.write("/random/setSeeds 5752"+str(j)+"3199 83812"+str(j)+"4975"+"\n")
+                f.write("/output/OutFile "+RadEl+"_"+ElementGems[i]+"_"+str(j)+"\n")
+                f.write("#Elements: Cathodes GEMs Rings Vessel Lens Sensors"+"\n")
+                f.write("/detector/RadElement "+RadEl+"\n")
+                f.write("/isotope/AtomicNumber "+str(ZElementGems[i])+"\n")
+                f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
+                f.write("/run/beamOn 100000"+"\n")
+
+
+
+
+def GEMsGenInner():
+
+    ElementGems=["K40"]
+    ZElementGems=[19]
+    NElements=[40]
+    RadEl="GEMsCore"
+    
+    for i in range(len(ElementGems)):
+        with open(RadEl+"_"+ElementGems[i]+".mac", 'w') as f:
+            f.write("/process/eLoss/StepFunction 0.01 0.1 mm"+"\n")
+            f.write("/run/verbose 0"+"\n")
+            f.write("/event/verbose 0"+"\n")
+            f.write("/random/setSeeds 575223199 8381234975"+"\n")
+            f.write("/output/OutFile "+RadEl+"_"+ElementGems[i]+"\n")
+            f.write("#Elements: Cathodes GEMs Rings Vessel Lens Sensors"+"\n")
+            f.write("/detector/RadElement "+RadEl+"\n")
+            f.write("/isotope/AtomicNumber "+str(ZElementGems[i])+"\n")
+            f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
+            f.write("/run/beamOn 10000000"+"\n")
+
+def GEMsGenOuterUTh():
+
+    ElementGems=["U238","Th232"]
+    ZElementGems=[92,90]
+    NElements=[238,232]
+    RadEl="GEMsCore"
+    
+    for i in range(len(ElementGems)):
+        for j in range(10):
+            with open(RadEl+'_'+ElementGems[i]+"_"+str(j)+".mac", 'w') as f:
+                f.write("/process/eLoss/StepFunction 0.01 0.1 mm"+"\n")
+                f.write("/run/verbose 0"+"\n")
+                f.write("/event/verbose 0"+"\n")
+                f.write("/random/setSeeds 5752"+str(j)+"3199 83812"+str(j)+"4975"+"\n")
+                f.write("/output/OutFile "+RadEl+"_"+ElementGems[i]+"_"+str(j)+"\n")
+                f.write("#Elements: Cathodes GEMs Rings Vessel Lens Sensors"+"\n")
+                f.write("/detector/RadElement "+RadEl+"\n")
+                f.write("/isotope/AtomicNumber "+str(ZElementGems[i])+"\n")
+                f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
+                f.write("/run/beamOn 100000"+"\n")
+
+
+
+            
+            
 def VesselGen():
 
     ElementGems=["U238","Th232","K40"]
@@ -80,6 +169,47 @@ def CathodesGen():
             f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
             f.write("/run/beamOn 10000000"+"\n")
 
+def CopperCathodesGenUTh():
+
+    ElementGems=["U238","Th232"]
+    ZElementGems=[92,90]
+    NElements=[238,232]
+    RadEl="Cathodes"
+    
+    for i in range(len(ElementGems)):
+        for j in range(10):
+            with open(RadEl+'_'+ElementGems[i]+"_"+str(j)+".mac", 'w') as f:
+                f.write("/process/eLoss/StepFunction 0.01 0.1 mm"+"\n")
+                f.write("/run/verbose 0"+"\n")
+                f.write("/event/verbose 0"+"\n")
+                f.write("/random/setSeeds 5752"+str(j)+"3199 83812"+str(j)+"4975"+"\n")
+                f.write("/output/OutFile "+RadEl+"_"+ElementGems[i]+"_"+str(j)+"\n")
+                f.write("#Elements: Cathodes GEMs Rings Vessel Lens Sensors"+"\n")
+                f.write("/detector/RadElement "+RadEl+"\n")
+                f.write("/isotope/AtomicNumber "+str(ZElementGems[i])+"\n")
+                f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
+                f.write("/run/beamOn 100000"+"\n")
+
+def CopperCathodesGen():
+
+    ElementGems=["K40","Co60","Cs137"]
+    ZElementGems=[19,27,55]
+    NElements=[40,60,137]
+    RadEl="Cathodes"
+    
+    for i in range(len(ElementGems)):
+        with open(RadEl+'_'+ElementGems[i]+".mac", 'w') as f:
+            f.write("/process/eLoss/StepFunction 0.01 0.1 mm"+"\n")
+            f.write("/run/verbose 0"+"\n")
+            f.write("/event/verbose 0"+"\n")
+            f.write("/random/setSeeds 575223199 8381234975"+"\n")
+            f.write("/output/OutFile "+RadEl+"_"+ElementGems[i]+"\n")
+            f.write("#Elements: Cathodes GEMs Rings Vessel Lens Sensors"+"\n")
+            f.write("/detector/RadElement "+RadEl+"\n")
+            f.write("/isotope/AtomicNumber "+str(ZElementGems[i])+"\n")
+            f.write("/isotope/MassNumber "+str(NElements[i])+"\n")
+            f.write("/run/beamOn 10000000"+"\n")
+                
 def LensGen():
 
     ElementGems=["U238","Th232"]
@@ -123,9 +253,7 @@ def SensorGen():
 
 if __name__ == "__main__":
 
-    GEMsGen()
-    VesselGen()
-    FieldCageGen()
-    CathodesGen()
-    LensGen()
-    SensorGen()
+    CopperCathodesGenUTh()
+    CopperCathodesGen()
+    
+    
